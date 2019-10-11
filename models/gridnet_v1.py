9070acdfe90ab3d929b411274929b26ff7198964
dangsuogui@foxmail.com <dang@dangdeMBP.local>
2019-10-11 13:24:23 +0800

    update model v1

--- models/gridnet_v1.py
+++ models/gridnet_v1.py
@@ -56,7 +56,10 @@ def get_model(point_cloud, is_training, bn_decay=None):
     key_in = tf.concat([tf.cos(key_in), tf.sin(key_in)], axis=2)
     value_in = tf.ones([batch_size, num_point, 1], dtype=tf.float32)
 
-    key_w = tf.Variable( tf.random_normal([super_vec_num, num_point, grid_num], stddev=0.1), name='w_keys', dtype=tf.float32 )
+    key_w = tf.Variable( tf.random_uniform([super_vec_num*num_point, 3], 0, 1), name='w_keys', dtype=tf.float32 )
+    key_w = tf.matmul( key_w, w_encode )
+    key_w = tf.reshape( key_w, [super_vec_num, num_point, -1] )
+    key_w = tf.concat([tf.cos(key_w), tf.sin(key_w)], axis=2)
     value_w = tf.Variable( tf.random_normal( [super_vec_num, num_point, 1], stddev=0.3), name='w_values', dtype=tf.float32 )
 
     ex_key_in = tf.reshape(key_in, [batch_size, 1, num_point, -1])
